bd8c6c9947c7eba26486462a11d6c1a7e83fe21b
Tanisha <tanishasura0@gmail.com>
2024-06-19 16:40:54 +0530

    fetch data from tmdb api

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import Results from "@/components/Results";
 
-export default function Home() {
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || 'fetchTrending';
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`
+    // `https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}&language=en-US&page=1`
+  );
+  const data = await res.json();
+  // console.log("data:", data); 
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  const results = data.results;
+console.log(results);
+
   return (
     <div>
-   Home
+      <Results results={results} />
     </div>
-  );
+  )
 }
